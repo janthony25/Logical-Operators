4927abef1e81d59f0f4b2d1e25e693d3f089f2d6
janthony25 <junanthony.business@gmail.com>
2024-01-30 23:05:22 +1300

    Add project files.

---
+++ Logical-Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LogicalOperators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Logical Operators
+            Console.WriteLine("What's the temperature outsie (C)");
+            double temp = Convert.ToDouble(Console.ReadLine());
+
+            if (temp >= 10 && temp <= 25)
+            {
+                Console.WriteLine("It is warm outside");
+            } 
+            else if (temp <= -50 || temp >= 50) {
+                Console.WriteLine("Do not go outside!");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
